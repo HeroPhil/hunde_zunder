b752cc26111a3f2a670641cfdc833562396e6762
herophil <philip.herold.01@gmail.com>
2021-12-09 15:00:27 +0100

    theme refinement

--- lib/constants/frontend/ui_theme.dart
+++ lib/constants/frontend/ui_theme.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class UiTheme {
   static final _primaryColor = Colors.orange;
   static final _secondaryColor = Colors.pink;
+  static final _primaryColorScheme =
+      ColorScheme.fromSwatch(primarySwatch: _primaryColor);
+
   static final lightTheme = ThemeData.from(
     colorScheme: ColorScheme.light(
       primary: _primaryColor,
--- lib/screens/home/pages/pet_page.dart
+++ lib/screens/home/pages/pet_page.dart
@@ -8,32 +8,47 @@ class PetPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          'PetPage',
-          style: Theme.of(context).textTheme.headline3,
-        ),
-        Expanded(
-          child: ListView(
-            children: [
-              ...context.read<PetProvider>().pets.map(
-                    (pet) => ListTile(
-                      leading: ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: CachedNetworkImage(
-                          imageUrl: pet.imageUrl,
-                          width: 50,
-                          height: 50,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Text(
+            'PetPage',
+            style: Theme.of(context).textTheme.headline3,
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: ListView(
+                children: [
+                  ...context.read<PetProvider>().pets.map(
+                        (pet) => ListTile(
+                          leading: ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: CachedNetworkImage(
+                              imageUrl: pet.imageUrl,
+                              width: 50,
+                              height: 50,
+                            ),
+                          ),
+                          title: Text(pet.name),
                         ),
                       ),
-                      title: Text(pet.name),
-                    ),
-                  ),
-            ],
+                ],
+              ),
+            ),
           ),
-        ),
-      ],
+          ElevatedButton(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Add Pet'),
+            ),
+            onPressed: () {
+              // context.read<PetProvider>().addPet();
+            },
+          ),
+        ],
+      ),
     );
   }
 }
