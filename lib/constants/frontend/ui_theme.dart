945b5141d7eb127e8e2279e90b05a099bd27b527
herophil <philip.herold.01@gmail.com>
2021-12-09 14:27:58 +0100

    updated theme

--- lib/constants/frontend/ui_theme.dart
+++ lib/constants/frontend/ui_theme.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 
 class UiTheme {
-  static final lightTheme = ThemeData(
-    primarySwatch: Colors.orange,
-    brightness: Brightness.light,
-    backgroundColor: Colors.white,
-    scaffoldBackgroundColor: Colors.white70,
+  static final _primaryColor = Colors.orange;
+  static final _secondaryColor = Colors.pink;
+  static final lightTheme = ThemeData.from(
+    colorScheme: ColorScheme.light(
+      primary: _primaryColor,
+      secondary: _secondaryColor,
+    ),
   );
 
-  static final darkTheme = ThemeData(
-    primarySwatch: Colors.orange,
-    brightness: Brightness.dark,
-    backgroundColor: Colors.black,
-    scaffoldBackgroundColor: Colors.black54,
+  static final darkTheme = ThemeData.from(
+    colorScheme: ColorScheme.dark(
+      primary: _primaryColor,
+      secondary: _secondaryColor,
+    ),
   );
 }
