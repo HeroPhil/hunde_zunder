52c9b8c55311903119235dcf293fe1682e1bc785
herophil <philip.herold.01@gmail.com>
2021-12-10 16:22:03 +0100

    updated SwipeCard Style

--- lib/screens/home/pages/swipe_page/widgets/swipe_card.dart
+++ lib/screens/home/pages/swipe_page/widgets/swipe_card.dart
@@ -14,24 +14,50 @@ class SwipeCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final frameSize = MediaQuery.of(context).size;
+    final theme = Theme.of(context);
+
     return Card(
       child: Container(
-        width: 100,
-        height: 200,
+        width: frameSize.width * 0.8,
+        height: frameSize.height * 0.6,
         color: Colors.accents.firstWhere(
           (_) => Random().nextDouble() < 0.2,
           orElse: () => Colors.redAccent,
         ),
         child: Stack(
           children: [
-            Text(pet.name),
             Positioned.fill(
-              child:
-                  //   CachedNetworkImage(
-                  //     imageUrl: pet.image,
-                  //     fit: BoxFit.cover,
-                  //   ),
-                  Image.memory(pet.image),
+              child: Image.memory(
+                pet.image,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Container(),
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(0, 0, 0, 0.6),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                    ),
+                  ),
+                  child: Text(
+                    pet.name,
+                    style: theme.textTheme.headline5!.copyWith(
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
