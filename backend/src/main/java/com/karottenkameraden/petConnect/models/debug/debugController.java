1eaf2fe7affd209cc6a6cd4bc32d5b1b744d0140
NicklasPlatz <57496386+NicklasPlatz@users.noreply.github.com>
2022-01-14 17:09:09 +0100

    Added token print to debug
    Co-Authored-By: MichaelKaiser1 <57496403+MichaelKaiser1@users.noreply.github.com>

--- backend/src/main/java/com/karottenkameraden/petConnect/models/debug/debugController.java
+++ backend/src/main/java/com/karottenkameraden/petConnect/models/debug/debugController.java
@@ -1,6 +1,13 @@
 package com.karottenkameraden.petConnect.models.debug;
 
+
+import java.util.Map;
+
+import com.karottenkameraden.petConnect.auth.models.User;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +16,22 @@ public class debugController {
     
     @GetMapping("/debug")
     public ResponseEntity<String> debug() {
-        return ResponseEntity.ok("Success");
+        String response = "SUCCESS <br/>";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User)authentication.getPrincipal();
+        response += "Uid: " + user.getUid() + "<br/>";
+        response += "Name: " + user.getName() + "<br/>";
+        response += "Email: " + user.getEmail() + "<br/>";
+        response += "isEmailVerified: " + user.isEmailVerified() + "<br/>";
+        response += "Issuer: " + user.getIssuer() + "<br/>";
+        response += "Picture: " + user.getPicture() + "<br/>";
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/public/debug")
     public ResponseEntity<String> publicDebug() {
-        return ResponseEntity.ok("Success");
+        String response = "SUCCESS";
+        return ResponseEntity.ok(response);
     }
 
 }
