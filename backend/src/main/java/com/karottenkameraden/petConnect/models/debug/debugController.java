813b86f534f70bed654254d9ac949b6b031b7195
NicklasPlatz <57496386+NicklasPlatz@users.noreply.github.com>
2022-01-21 16:43:14 +0100

    Updated Endpoints
    Co-Authored-By: MichaelKaiser1 <57496403+MichaelKaiser1@users.noreply.github.com>

--- backend/src/main/java/com/karottenkameraden/petConnect/models/debug/debugController.java
+++ backend/src/main/java/com/karottenkameraden/petConnect/models/debug/debugController.java
@@ -14,24 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class debugController {
     
-    @GetMapping("/debug")
-    public ResponseEntity<String> debug() {
-        String response = "SUCCESS <br/>";
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user = (User)authentication.getPrincipal();
-        response += "Uid: " + user.getUid() + "<br/>";
-        response += "Name: " + user.getName() + "<br/>";
-        response += "Email: " + user.getEmail() + "<br/>";
-        response += "isEmailVerified: " + user.isEmailVerified() + "<br/>";
-        response += "Issuer: " + user.getIssuer() + "<br/>";
-        response += "Picture: " + user.getPicture() + "<br/>";
-        return ResponseEntity.ok(response);
-    }
+    // @GetMapping("/debug")
+    // public ResponseEntity<String> debug() {
+    //     String response = "SUCCESS <br/>";
+    //     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    //     User user = (User)authentication.getPrincipal();
+    //     response += "Uid: " + user.getUid() + "<br/>";
+    //     response += "Name: " + user.getName() + "<br/>";
+    //     response += "Email: " + user.getEmail() + "<br/>";
+    //     response += "isEmailVerified: " + user.isEmailVerified() + "<br/>";
+    //     response += "Issuer: " + user.getIssuer() + "<br/>";
+    //     response += "Picture: " + user.getPicture() + "<br/>";
+    //     return ResponseEntity.ok(response);
+    // }
 
-    @GetMapping("/public/debug")
-    public ResponseEntity<String> publicDebug() {
-        String response = "SUCCESS";
-        return ResponseEntity.ok(response);
-    }
+    // @GetMapping("/public/debug")
+    // public ResponseEntity<String> publicDebug() {
+    //     String response = "SUCCESS";
+    //     return ResponseEntity.ok(response);
+    // }
 
 }
