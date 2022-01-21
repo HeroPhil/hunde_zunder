f35ef550872b50afe13389788d13c2015532befa
herophil <philip.herold.01@gmail.com>
2022-01-21 13:27:00 +0100

    added more fields to Pet model

--- lib/models/pet.dart
+++ lib/models/pet.dart
@@ -2,16 +2,50 @@ import 'dart:typed_data';
 
 import 'package:uuid/uuid.dart';
 
+enum PetType {
+  dog,
+  cat,
+  bird,
+  fish,
+  other,
+}
+
 class Pet {
-  // Mocked
-  late final String id;
-  final String name;
-  final Uint8List image;
+  late final String id; // Mocked
+  String name;
+  Uint8List image;
+  PetType type;
+  String? race;
+  String? description;
+  DateTime? birthday;
 
   Pet({
     required this.name,
     required this.image,
-  }) {
-    id = Uuid().v4();
-  }
+    this.type = PetType.other,
+    this.race,
+    this.description,
+    this.birthday,
+  }) : id = Uuid().v4();
+
+  Pet.fromJson({
+    required this.id,
+    required Map<String, dynamic> json,
+  })  : name = json["name"],
+        image = Uint8List.fromList(json["image"]),
+        type = PetType.values[json["type"] ?? 4],
+        race = json["race"],
+        description = json["description"],
+        birthday = json["birthday"] != null
+            ? DateTime.fromMillisecondsSinceEpoch(json["birthday"])
+            : null;
+
+  get asJson => {
+        "name": name,
+        "image": image,
+        "type": PetType.values.indexOf(type),
+        if (race != null) "race": race,
+        if (description != null) "description": description,
+        if (birthday != null) "birthday": birthday!.millisecondsSinceEpoch,
+      };
 }
