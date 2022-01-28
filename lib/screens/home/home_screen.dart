b92926c13c2e7b9bd6dfb24d4d65a247b47dd86e
herophil <philip.herold.01@gmail.com>
2022-01-28 10:17:34 +0100

    updated homescreen appbar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,10 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:hunde_zunder/constants/frontend/ui_theme.dart';
 import 'package:hunde_zunder/screens/home/home_screen_provider.dart';
 import 'package:hunde_zunder/screens/home/pages/match/match_page.dart';
 import 'package:hunde_zunder/screens/home/pages/pet/pet_page.dart';
@@ -11,7 +15,7 @@ import 'package:hunde_zunder/services/backend_service.dart';
 import 'package:hunde_zunder/services/firebase_auth_service.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/src/provider.dart';
-import 'package:res_builder/responsive.dart';
+import 'package:res_builder/responsive.dart' as rb;
 
 class HomeScreen extends StatelessWidget {
   static const routeName = '/home';
@@ -19,19 +23,45 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Consumer<HomeScreenProvider>(
       builder: (context, homeScreenProvider, _) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Pet Connect'),
-          actions: [
-            IconButton(
-              onPressed: () =>
-                  Navigator.pushNamed(context, ProfileScreen.routeName),
-              icon: const Icon(Icons.person),
-            )
-          ],
+        appBar: CupertinoNavigationBar(
+          backgroundColor: Colors.transparent,
+          border: Border.symmetric(),
+          middle: GestureDetector(
+            onTap: () => Navigator.of(context)
+              ..popUntil((route) => route.isFirst)
+              ..pushReplacementNamed(HomeScreen.routeName),
+            child: Text(
+              'Pet Connect',
+              style: theme.textTheme.headline3!.copyWith(
+                color: UiTheme.primaryColor,
+              ),
+            ),
+          ),
+          trailing: Builder(
+            builder: (context) {
+              final user = context.read<User>();
+              final label =
+                  user.displayName ?? user.email ?? "Who the fuck are u?";
+              return TextButton.icon(
+                label: Text(
+                  rb.Responsive.value<String>(
+                    context: context,
+                    onMobile: "",
+                    onTablet: label,
+                  ),
+                ),
+                onPressed: () =>
+                    Navigator.pushNamed(context, ProfileScreen.routeName),
+                icon: const Icon(Icons.person),
+              );
+            },
+          ),
         ),
-        body: Responsive.withShared<List<Widget>>(
+        body: rb.Responsive.withShared<List<Widget>>(
           share: [
             const PetPage(),
             const SwipePage(),
@@ -50,7 +80,7 @@ class HomeScreen extends StatelessWidget {
             children: children,
           ),
         ),
-        bottomNavigationBar: Responsive.isMobile(context)
+        bottomNavigationBar: rb.Responsive.isMobile(context)
             ? BottomNavigationBar(
                 currentIndex: (homeScreenProvider.pageController.hasClients
                         ? homeScreenProvider.pageController.page ?? 1
