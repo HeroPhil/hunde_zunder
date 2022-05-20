5df10dd1fdc4090ba1f618322d7919a4b4de9e41
herophil <philip.herold.01@gmail.com>
2022-05-20 10:43:38 +0200

    updated backend service

--- lib/services/backend_service.dart
+++ lib/services/backend_service.dart
@@ -6,6 +6,13 @@ import 'package:http/http.dart' as http;
 
 import 'dart:html';
 
+enum RequestType {
+  GET,
+  POST,
+  PUT,
+  DELETE,
+}
+
 class BackendService with ChangeNotifier {
   // static final url = "api.pet-connect.karottenkameraden.de";
   // static final url = Uri.parse(
@@ -30,9 +37,11 @@ class BackendService with ChangeNotifier {
   });
 
   dynamic callBackend({
+    required RequestType requestType,
     String? endpoint,
     Map<String, dynamic>? queryParameters,
     Map<String, String>? headerParameters,
+    Map<String, dynamic>? body,
   }) async {
     final url = baseUrl.replace(
       path: endpoint != null ? "${baseUrl.path}/$endpoint" : baseUrl.path,
@@ -47,12 +56,30 @@ class BackendService with ChangeNotifier {
       print("token for backend auth: $token");
     }
 
-    final response = await http.get(url, headers: {
+    final headers = {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
       if (token != null) 'Authorization': 'Bearer $token',
       if (headerParameters != null) ...headerParameters,
-    });
+    };
+
+    late http.Response response;
+    switch (requestType) {
+      case RequestType.POST:
+        response =
+            await http.post(url, headers: headers, body: json.encode(body));
+        break;
+      case RequestType.PUT:
+        response =
+            await http.put(url, headers: headers, body: json.encode(body));
+        break;
+      case RequestType.DELETE:
+        response = await http.delete(url, headers: headers);
+        break;
+      default:
+        response = await http.get(url, headers: headers);
+        break;
+    }
 
     print("response(${response.statusCode}): ${response.body}");
 
