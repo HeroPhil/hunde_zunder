bddfd155fcae21dcc9d1f3b43c39231f3846763d
Michael Kaiser <michi.kaiser2001@gmail.com>
2022-06-03 11:30:22 +0200

    corrected controller.js

--- backend/controller.js
+++ backend/controller.js
@@ -9,15 +9,15 @@ const router = Router()
 // DEBUG ENDPOINTS
 // =================
 
-router.get('/', (req,res) => { 
+router.get('/', (req, res) => {
     res.status(200).send('Hello Petty World')
 })
 
-router.get('/me', checkIfAuthenticated, (req,res) => {
+router.get('/me', checkIfAuthenticated, (req, res) => {
     res.status(200).send(`You are authenticated with authID = ${req.authId}`)
 })
 
-router.get('/db2/:id',  async (req,res) => {
+router.get('/db2/:id', async(req, res) => {
     const { id } = req.params
     x = await sampleQuery2(id)
     res.status(200).send(x)
@@ -29,34 +29,34 @@ router.get('/db2/:id',  async (req,res) => {
 // =================
 
 // Return pets under the requesting Owner
-router.get('/pets', checkIfAuthenticated, async (req,res) => {
+router.get('/pets', checkIfAuthenticated, async(req, res) => {
     ownerId = req.authId
     pets = await getMyPets(ownerId)
     res.status(200).send(pets)
 })
 
 // Returns the requested pet
-router.get('/pets/:id', checkIfAuthenticated, async (req,res) => {
+router.get('/pets/:id', checkIfAuthenticated, async(req, res) => {
     const { id } = req.params
     pet = await getPetById(id)
     res.status(200).send(pet)
 })
 
 // Updates the specified pet of the requesting owner
-router.put('/pets/:id', checkIfAuthenticated, async (req,res) => {
+router.put('/pets/:id', checkIfAuthenticated, async(req, res) => {
     const { id } = req.params
     answer = await updatePet(id, req.body.name, req.body.image, req.body.type, req.body.gender, req.body.race, req.body.description, req.body.birthday, req.authId)
     res.status(200).send(answer)
 })
 
 // Creates a new belonging to the requesting owner
-router.post('/pets', checkIfAuthenticated, async (req,res) => {
+router.post('/pets', checkIfAuthenticated, async(req, res) => {
     answer = await createPet(req.body.name, req.body.image, req.body.type, req.body.gender, req.body.race, req.body.description, req.body.birthday, req.authId)
     res.status(200).send(answer)
 })
 
 // Deletes the specified pet of the requesting owner
-router.delete('/pets/:id', checkIfAuthenticated, async (req,res) => {
+router.delete('/pets/:id', checkIfAuthenticated, async(req, res) => {
     const { id } = req.params
     answer = await deletePet(id, req.authId)
     res.status(200).send(answer)
@@ -70,7 +70,7 @@ router.delete('/pets/:id', checkIfAuthenticated, async (req,res) => {
 // =================
 
 // Returns the requested match
-router.get('/match/:id', checkIfAuthenticated, async (req,res) => {
+router.get('/match/:id', checkIfAuthenticated, async(req, res) => {
     const { id } = req.params
     match = await getMatchById(id)
     res.status(200).send(match)
@@ -79,11 +79,11 @@ router.get('/match/:id', checkIfAuthenticated, async (req,res) => {
 // Returns matches for swiping
 // Priority: started but not completed matches -> matches were the pet has been swiped -> new matches
 // New matches -> up to five new matches are created
-router.get('/matches/:petId', checkIfAuthenticated, async (req,res) => {
+router.get('/matches/:petId', checkIfAuthenticated, async(req, res) => {
     const { petId } = req.params
     ownerId = req.authId
     pet = getPetById(petId)
-    if(pet["ownerID"] != ownerId){
+    if (pet["ownerID"] != ownerId) {
         res.status(403).send("Prohibited")
         return
     }
@@ -92,7 +92,7 @@ router.get('/matches/:petId', checkIfAuthenticated, async (req,res) => {
 })
 
 // Updates the specified match as long as the requesting owner is the owner of one of the two pets
-router.put('/match/:id', checkIfAuthenticated, async (req,res) => {
+router.put('/match/:id', checkIfAuthenticated, async(req, res) => {
     const { id } = req.params
     match = await updateMatchById(id)
     res.status(200).send(match)
@@ -107,20 +107,21 @@ router.put('/match/:id', checkIfAuthenticated, async (req,res) => {
 
 // Support method realizing the priority of match selection for swiping
 // Used in: '/matches/:petId'
-const getNewMatches = async (petId, ownerId) => {
+const getNewMatches = async(petId, ownerId) => {
     matches = await getOpenMatches(petId)
-    if(matches.size <= 0) {
+    if (matches.size <= 0) {
         matches = await getPotentialMatches(petId)
 
-        if(matches.size <= 0) {
+        if (matches.size <= 0) {
             pets = await getPotentialPets(ownerId, petId)
-            if(pets.size <= 0) {
+            if (pets.size <= 0) {
                 return matches
             }
-            pets.forEach(pet => {
-                swipeeId = pet["petID"]
-                await createMatch(petId, swipeeId)
-            });
+            await Promise.all(
+                pets.map((pet) => {
+                    swipeeId = pet["petID"]
+                    return createMatch(petId, swipeeId)
+                }));
             return getNewMatches(petId, ownerId)
         }
     }
