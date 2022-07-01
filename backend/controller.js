40f3fe79a1cfbe94949cd9b36b502332f5c71cef
Christian Bettermann <christian.bettermann@atos.net>
2022-07-01 16:27:29 +0200

    Added swagger entries

--- backend/controller.js
+++ backend/controller.js
@@ -45,9 +45,9 @@ router.get('/db2/:id', async (req, res) => {
     ### Swagger Documentation
     #swagger.tags = ["test"] 
     #swagger.description = 'This is a test endpoint to receive sample entries from our test database.'
-    #swagger.parameters['name'] = { 
+    #swagger.parameters['id'] = { 
         in: 'body', 
-        type: 'int', 
+        type: 'string', 
         description: 'The id of the requested sample entry.' 
     }
     #swagger.responses[200] = { 
@@ -85,6 +85,23 @@ router.get('/pets/:id', checkIfAuthenticated, async (req, res) => {
     const { id } = req.params
     pet = await getPetById(id)
     res.status(200).send(pet)
+
+    /* 
+    ### Swagger Documentation
+    #swagger.tags = ["pets"] 
+    #swagger.description = 'Endpoint to get all data points about the requested pet.'
+    #swagger.security = [{
+            "bearerAuth": []
+        }]
+    #swagger.parameters['id'] = { 
+        in: 'body', 
+        type: 'string', 
+        description: 'The id of the requested pet entry.' 
+    }
+    #swagger.responses[200] = { 
+        description: "Your pet was found in the database and all data points are displayed to you.", 
+    }    
+    */
 })
 
 // Updates the specified pet of the requesting owner
@@ -92,12 +109,108 @@ router.put('/pets/:id', checkIfAuthenticated, async (req, res) => {
     const { id } = req.params
     answer = await updatePet(id, req.body.name, req.body.image, req.body.type, req.body.gender, req.body.race, req.body.description, req.body.birthday, req.authId)
     res.status(200).send(answer)
+
+    /* 
+    ### Swagger Documentation
+    #swagger.tags = ["pets"] 
+    #swagger.description = 'Endpoint to update data points of a specific pet.'
+    #swagger.security = [{
+            "bearerAuth": []
+        }]
+    #swagger.parameters['id'] = { 
+        in: 'body', 
+        type: 'string', 
+        description: 'The id of the pet entry.' 
+    }
+    #swagger.parameters['name'] = { 
+        in: 'body', 
+        type: 'string', 
+        description: 'The name of the pet.' 
+    }
+    #swagger.parameters['image'] = { 
+        in: 'body', 
+        type: 'image', 
+        description: 'The image of the pet.' 
+    }
+    #swagger.parameters['type'] = { 
+        in: 'body', 
+        type: 'string', 
+        description: 'The pet type of the pet.' 
+    }
+    , req.body., req.body., req.body.birthday
+    #swagger.parameters['gender'] = { 
+        in: 'body', 
+        type: 'string', 
+        description: 'The gender of the pet.' 
+    }
+    #swagger.parameters['race'] = { 
+        in: 'body', 
+        type: 'string', 
+        description: 'The race of the pet.' 
+    }
+    #swagger.parameters['type'] = { 
+        in: 'body', 
+        type: 'string', 
+        description: 'The type of pet.' 
+    }
+    #swagger.responses[200] = { 
+        description: "Your pet was found in the database and the data points were updated.", 
+    }    
+    */
 })
 
 // Creates a new belonging to the requesting owner
 router.post('/pets', checkIfAuthenticated, async (req, res) => {
     answer = await createPet(req.body.name, req.body.image, req.body.type, req.body.gender, req.body.race, req.body.description, req.body.birthday, req.authId)
     res.status(200).send(answer)
+
+    /* 
+    ### Swagger Documentation
+    #swagger.tags = ["pets"] 
+    #swagger.description = 'Endpoint to create a new pet in the database.'
+    #swagger.security = [{
+            "bearerAuth": []
+        }]
+    #swagger.parameters['id'] = { 
+        in: 'body', 
+        type: 'string', 
+        description: 'The id of the pet entry.' 
+    }
+    #swagger.parameters['name'] = { 
+        in: 'body', 
+        type: 'string', 
+        description: 'The name of the pet.' 
+    }
+    #swagger.parameters['image'] = { 
+        in: 'body', 
+        type: 'image', 
+        description: 'The image of the pet.' 
+    }
+    #swagger.parameters['type'] = { 
+        in: 'body', 
+        type: 'string', 
+        description: 'The pet type of the pet.' 
+    }
+    , req.body., req.body., req.body.birthday
+    #swagger.parameters['gender'] = { 
+        in: 'body', 
+        type: 'string', 
+        description: 'The gender of the pet.' 
+    }
+    #swagger.parameters['race'] = { 
+        in: 'body', 
+        type: 'string', 
+        description: 'The race of the pet.' 
+    }
+    #swagger.parameters['type'] = { 
+        in: 'body', 
+        type: 'string', 
+        description: 'The type of pet.' 
+    }
+    #swagger.responses[200] = { 
+        description: "Your pet was found in the database and the data points were updated.", 
+    }    
+    */
 })
 
 // Deletes the specified pet of the requesting owner
@@ -105,6 +218,23 @@ router.delete('/pets/:id', checkIfAuthenticated, async (req, res) => {
     const { id } = req.params
     answer = await deletePet(id, req.authId)
     res.status(200).send(answer)
+
+    /* 
+    ### Swagger Documentation
+    #swagger.tags = ["pets"] 
+    #swagger.description = 'Endpoint to delte all data points about the pet.'
+    #swagger.security = [{
+            "bearerAuth": []
+        }]
+    #swagger.parameters['id'] = { 
+        in: 'body', 
+        type: 'string', 
+        description: 'The id of the pet entry.' 
+    }
+    #swagger.responses[200] = { 
+        description: "Your pet was found and deleted from the database", 
+    }    
+    */
 })
 
 
@@ -119,6 +249,23 @@ router.get('/match/:id', checkIfAuthenticated, async (req, res) => {
     const { id } = req.params
     match = await getMatchById(id)
     res.status(200).send(match)
+
+    /* 
+    ### Swagger Documentation
+    #swagger.tags = ["match"] 
+    #swagger.description = 'Endpoint to get all data points about the requested match.'
+    #swagger.security = [{
+            "bearerAuth": []
+        }]
+    #swagger.parameters['id'] = { 
+        in: 'body', 
+        type: 'string', 
+        description: 'The id of the match entry.' 
+    }
+    #swagger.responses[200] = { 
+        description: "Your match was found in the database and is displayed to you.", 
+    }    
+    */
 })
 
 // Returns matches for swiping
@@ -135,6 +282,23 @@ router.get('/matches/:petId', checkIfAuthenticated, async (req, res) => {
     }
     matches = await getNewMatches(petId, ownerId)
     res.status(200).send(matches)
+
+    /* 
+    ### Swagger Documentation
+    #swagger.tags = ["match"] 
+    #swagger.description = 'Endpoint to get matches for swiping.'
+    #swagger.security = [{
+            "bearerAuth": []
+        }]
+    #swagger.parameters['petId'] = { 
+        in: 'body', 
+        type: 'string', 
+        description: 'The id of the pet you want open matches for.' 
+    }
+    #swagger.responses[200] = { 
+        description: "An open match was found in the database and is displayed to you.", 
+    }    
+    */
 })
 
 // Updates the specified match as long as the requesting owner is the owner of one of the two pets
@@ -142,6 +306,48 @@ router.put('/match/:id', checkIfAuthenticated, async (req, res) => {
     const { id } = req.params
     match = await updateMatchById(req.authId, id, req.body.swiperId, req.body.swipeeId, req.body.request, req.body.answer, req.body.matchDate)
     res.status(200).send(match)
+
+    /* 
+    ### Swagger Documentation
+    #swagger.tags = ["match"] 
+    #swagger.description = 'Endpoint to update matches.'
+    #swagger.security = [{
+            "bearerAuth": []
+        }]
+    #swagger.parameters['id'] = { 
+        in: 'body', 
+        type: 'string', 
+        description: 'The id of the match you want to update.' 
+    }
+    #swagger.parameters['swiperId'] = { 
+        in: 'body', 
+        type: 'string', 
+        description: 'The id of the swiper.' 
+    }
+    #swagger.parameters['swipeeId'] = { 
+        in: 'body', 
+        type: 'string', 
+        description: 'The id of the swipee.' 
+    }
+    #swagger.parameters['request'] = { 
+        in: 'body', 
+        type: 'boolean', 
+        description: 'Set the new request value.' 
+    }
+    #swagger.parameters['answer'] = { 
+        in: 'body', 
+        type: 'boolean', 
+        description: 'Set the new answer value.' 
+    }
+    #swagger.parameters['matchDate'] = { 
+        in: 'body', 
+        type: 'string', 
+        description: 'Set the date when the match is closed.' 
+    }
+    #swagger.responses[200] = { 
+        description: "The match was successfully updated.", 
+    }    
+    */
 })
 
 
