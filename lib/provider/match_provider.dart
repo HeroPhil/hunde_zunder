69313feceac4a19e58ec11d77b90678b53a6eb4c
herophil <philip.herold.01@gmail.com>
2022-07-13 14:53:44 +0200

    protected matches with mutex

--- lib/provider/match_provider.dart
+++ lib/provider/match_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hunde_zunder/constants/backend/api_endpoints.dart';
 import 'package:hunde_zunder/services/backend_service.dart';
+import 'package:mutex/mutex.dart';
 import 'package:provider/provider.dart';
 import '../models/pet.dart';
 import '../models/match.dart' as Model;
@@ -9,6 +10,8 @@ import 'pet_provider.dart';
 import 'mock_provider.dart';
 
 class MatchProvider with ChangeNotifier {
+  static final _mutex = Mutex();
+
   //dependencies
   final BackendService backendService;
   final PetProvider petProvider;
@@ -39,25 +42,28 @@ class MatchProvider with ChangeNotifier {
   }
 
   List<Model.Match>? get matches {
-    if (_matches == null) {
-      final myPetIds =
-          petProvider.myPets?.map((pet) => pet.petID.toString()).toList() ?? [];
+    if (!_mutex.isLocked && _matches == null) {
+      _mutex.protect(() {
+        final myPetIds =
+            petProvider.myPets?.map((pet) => pet.petID.toString()).toList() ??
+                [];
 
-      final backendFutures = myPetIds.map(
-        (petId) => backendService
-            .callBackend(
-          requestType: RequestType.GET,
-          endpoint: ApiEndpoints.allMatchesForPet(petId),
-          jsonParser: (json) => Model.Match.fromJson(json),
-        )
-            .then(
-          (matches) {
-            (_matches ??= []).addAll(matches);
-          },
-        ),
-      );
+        final backendFutures = myPetIds.map(
+          (petId) => backendService
+              .callBackend(
+            requestType: RequestType.GET,
+            endpoint: ApiEndpoints.allMatchesForPet(petId),
+            jsonParser: (json) => Model.Match.fromJson(json),
+          )
+              .then(
+            (matches) {
+              (_matches ??= []).addAll(matches);
+            },
+          ),
+        );
 
-      Future.wait(backendFutures).then((value) => notifyListeners());
+        return Future.wait(backendFutures).then((value) => notifyListeners());
+      });
     }
     return _matches;
   }
