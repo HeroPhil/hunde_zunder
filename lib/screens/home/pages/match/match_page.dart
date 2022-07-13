efe72d9824218fb22a93dd789affff13ff17cf08
herophil <philip.herold.01@gmail.com>
2022-07-13 19:19:38 +0200

    froze pageview

--- lib/screens/home/pages/match/match_page.dart
+++ lib/screens/home/pages/match/match_page.dart
@@ -12,66 +12,72 @@ class MatchPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: PageView.builder(
-        controller: context.read<MatchPageProvider>().pageController,
-        itemCount: 2,
-        itemBuilder: (context, index) {
-          switch (index) {
-            case 1:
-              return ChatPage();
-            default:
-              return Column(
-                children: [
-                  Text(
-                    'My Matches',
-                    style: Theme.of(context).textTheme.headline3,
-                  ),
-                  Consumer<MatchPageProvider>(
-                    builder: (context, matchPageProvider, _) {
-                      final matches = matchPageProvider.matches;
-                      if (matches == null) {
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-                      if (matches.isEmpty) {
-                        const Center(
-                          child: Text('No matches found'), // TODO add lottie
-                        );
-                      }
+    return Consumer<MatchPageProvider>(
+      builder: (context, matchPageProvider, child) => Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: PageView.builder(
+          // physics: (matchPageProvider.pageController.page ?? 1) > 0.0
+          //     ? NeverScrollableScrollPhysics()
+          //     : null,
+          physics: const NeverScrollableScrollPhysics(),
+          controller: matchPageProvider.pageController,
+          itemCount: 2,
+          itemBuilder: (context, index) {
+            switch (index) {
+              case 1:
+                return ChatPage();
+              default:
+                return Column(
+                  children: [
+                    Text(
+                      'My Matches',
+                      style: Theme.of(context).textTheme.headline3,
+                    ),
+                    Builder(
+                      builder: (context) {
+                        final matches = matchPageProvider.matches;
+                        if (matches == null) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                        if (matches.isEmpty) {
+                          const Center(
+                            child: Text('No matches found'), // TODO add lottie
+                          );
+                        }
 
-                      return Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 8.0),
-                          child: ListView(
-                            children: [
-                              ...matchPageProvider.matches!.map(
-                                (match) => GestureDetector(
-                                  onTap: () {
-                                    context.read<ChatPageProvider>().init(
-                                          match: match,
-                                          popBehavior: () =>
-                                              matchPageProvider.page = 0,
-                                        );
-                                    matchPageProvider.page = 1;
-                                  },
-                                  child: MatchCard(
-                                    match: match,
+                        return Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: ListView(
+                              children: [
+                                ...matchPageProvider.matches!.map(
+                                  (match) => GestureDetector(
+                                    onTap: () {
+                                      context.read<ChatPageProvider>().init(
+                                            match: match,
+                                            popBehavior: () =>
+                                                matchPageProvider.page = 0,
+                                          );
+                                      matchPageProvider.page = 1;
+                                    },
+                                    child: MatchCard(
+                                      match: match,
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              );
-          }
-        },
+                        );
+                      },
+                    ),
+                  ],
+                );
+            }
+          },
+        ),
       ),
     );
   }
