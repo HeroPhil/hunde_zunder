87be3135b2aaa3416afadcd956654718de5c5368
herophil <philip.herold.01@gmail.com>
2022-07-13 20:10:34 +0200

    updated swipe backend call in frontend

--- lib/screens/home/pages/swipe/swipe_page_provider.dart
+++ lib/screens/home/pages/swipe/swipe_page_provider.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:hunde_zunder/constants/backend/api_endpoints.dart';
 import 'package:hunde_zunder/services/backend_service.dart';
 import '../../../../models/pet.dart';
-import '../../../../models/match.dart';
+import '../../../../models/match.dart' as Model;
 import '../../../../provider/pet_provider.dart';
 
 enum SwipeResult {
@@ -19,7 +19,7 @@ class SwipePageProvider with ChangeNotifier {
   final BackendService backendService;
 
   // cache
-  List<Match>? _matches;
+  List<Model.Match>? _matches;
   Pet? _candidate;
 
   SwipePageProvider({
@@ -43,7 +43,7 @@ class SwipePageProvider with ChangeNotifier {
     super.dispose();
   }
 
-  List<Match>? get matches {
+  List<Model.Match>? get matches {
     if (_matches == null) {
       if (petProvider.currentPet == null) {
         print("ERR: no pet selected");
@@ -51,12 +51,12 @@ class SwipePageProvider with ChangeNotifier {
       }
 
       backendService
-          .callBackend<Match>(
+          .callBackend<Model.Match>(
         requestType: RequestType.GET,
         endpoint: ApiEndpoints.newMatchForId(
           petProvider.currentPet!.petID.toString(),
         ),
-        jsonParser: (json) => Match.fromJson(json),
+        jsonParser: (json) => Model.Match.fromJson(json),
       )
           .then((matches) {
         (_matches ??= []).addAll(matches);
@@ -73,7 +73,7 @@ class SwipePageProvider with ChangeNotifier {
       }
 
       final foreignPetID =
-          matches!.first.swipeeID == petProvider.currentPet?.petID
+          currentMatch!.swipeeID == petProvider.currentPet?.petID
               ? matches!.first.swiperID
               : matches!.first.swipeeID;
       petProvider
@@ -85,14 +85,17 @@ class SwipePageProvider with ChangeNotifier {
     return _candidate;
   }
 
+  Model.Match? get currentMatch => matches?.first;
+
   void swipeCard(SwipeResult result) async {
-    if (result == SwipeResult.love) {
-      // make love
-      print("making love");
-    } else {
-      // kick in ass
-      print("kicking in ass");
-    }
+    final boolResult = result == SwipeResult.love;
+
+    final isSwipee = currentMatch!.swipeeID == petProvider.currentPet!.petID;
+
+    Model.Match updatedMatch = currentMatch!.copyWith(
+      answer: isSwipee ? boolResult : currentMatch!.answer,
+      request: isSwipee ? currentMatch!.request : boolResult,
+    );
 
     // ? wait for backend to respond before clearing cache
     // ! yes because otherwise race condition might return the same match again
@@ -101,7 +104,10 @@ class SwipePageProvider with ChangeNotifier {
     // TODO maybe their needs to be a page wide loading control?
     await backendService.callBackend(
       requestType: RequestType.PUT,
-      endpoint: ApiEndpoints.matchById(matches!.first.matchID.toString()),
+      endpoint: ApiEndpoints.matchById(
+        matches!.first.matchID.toString(),
+      ),
+      body: updatedMatch.toJson,
     );
 
     // either Way, remove from stack
