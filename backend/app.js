8a021cbc9ae15bda2b9708abe528f5d5aa643fe0
Christian Bettermann <christian.bettermann@atos.net>
2022-07-25 23:15:19 +0200

    Swagger fix

--- backend/app.js
+++ backend/app.js
@@ -5,35 +5,33 @@ const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT
 
-// app.use(express.json())
+app.use(express.json())
 
-// const swaggerAutogen = require("swagger-autogen")();
-// const outputFile = "./swagger-output.json";
-// const endpointsFiles = ["./controller/petController.js", "./controller/matchController.js", "./controller/chatController.js"];
-// const config = {
-//     securityDefinitions: {
-//         bearerAuth: {
-//             type: 'http',
-//             scheme: 'bearer',
-//             bearerFormat: 'JWT'
-//         }
-//     }
-// };
-// swaggerAutogen(outputFile, endpointsFiles, config).then(
-//     async() => {
-//         await
-//         import ('./app.js');
-//     });
+const swaggerAutogen = require('swagger-autogen')();
+const outputFile = './backend/swagger-output.json';
+//const endpointsFiles = ["./backend/controller/petController.js", "./backend/controller/matchController.js", "./backend/controller/chatController.js"];
+const endpointsFiles = ['./backend/app.js']; // root file where the route starts.
 
+const config = {
+    securityDefinitions: {
+        bearerAuth: {
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'JWT'
+        }
+    }
+};
+
+swaggerAutogen(outputFile, endpointsFiles, config);
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 
 
 app.use(cors({ credentials: true, origin: true }))
 
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = require('./swagger-output.json');
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+ const swaggerUi = require('swagger-ui-express');
+ const swaggerDocument = './swagger-output.json';
+ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const debugController = require('./controller/debugController')
 app.use('/', debugController)
